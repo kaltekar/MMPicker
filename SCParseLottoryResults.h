6edb504e602399deee8d33e2d05a4dc72996f9be
Reed Stoner <kaltekar@gmail.com>
2012-02-04 15:52:51 -0500

    Added Parser class

---
+++ SCParseLottoryResults.h
@@ -0,0 +1,13 @@
+//
+//  SCParseLottoryResults.h
+//  MegaMillionsPicker
+//
+//  Created by Reed Stoner on 2/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCParseLottoryResults : NSObject
+
+@end
---
+++ SCParseLottoryResults.m
@@ -0,0 +1,13 @@
+//
+//  SCParseLottoryResults.m
+//  MegaMillionsPicker
+//
+//  Created by Reed Stoner on 2/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SCParseLottoryResults.h"
+
+@implementation SCParseLottoryResults
+
+@end
